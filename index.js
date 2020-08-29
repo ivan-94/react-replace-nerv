b6c90b8b76cfff3fa7946790f5e7a3dd17a96f08
黎贵奇 <carney520@hotmail.com>
2020-08-29 16:01:34 +0800

    feat: base support

--- index.js
+++ index.js
@@ -1,21 +1,119 @@
-const react = require("react");
-const reactDom = require("react-dom");
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Taro = require('@tarojs/taro');
 
-console.log(reactDom)
+const Nerv = {};
 
-const Nerv = {
-  ...react,
-  ...reactDom,
+Object.assign(Nerv, React, ReactDOM);
+
+// 改写 Owner
+// 这种行为依赖于 React 的底层行为，所以不推荐
+const ReactCurrentOwner = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner;
+delete ReactCurrentOwner.current;
+let currentOwnner;
+Object.defineProperty(ReactCurrentOwner, 'current', {
+  get() {
+    return currentOwnner;
+  },
+  set(v) {
+    currentOwnner = v;
+    if (v && v.stateNode) {
+      v.stateNode.hooks = v.stateNode.hooks || [];
+      Taro.Current.current = v.stateNode;
+      Taro.Current.index = 0;
+    }
+  },
+});
+
+function getReactOwner() {
+  return ReactCurrentOwner.current;
+}
+
+const HOOKS = [
+  'useCallback',
+  'useContext',
+  'useDebugValue',
+  'useEffect',
+  'useImperativeHandle',
+  'useLayoutEffect',
+  'useMemo',
+  'useRef',
+  'useState',
+  'useReducer',
+];
+
+HOOKS.forEach((hook) => {
+  Nerv[hook] = function () {
+    const owner = getReactOwner();
+    if (owner == null) {
+      throw new Error('请在组件内使用 Hooks');
+    }
+
+    // 类组件, Taro 2.x 会将函数式组件转换为 类组件，所以一般走这里
+    if (owner.stateNode) {
+      Taro[hook].apply(null, arguments);
+    } else {
+      // 函数组件
+      React[hook].apply(null, arguments);
+    }
+  };
+});
+
+Nerv.nextTick = function (fn) {
+  var args = [],
+    len = arguments.length - 1;
+  while (len-- > 0) args[len] = arguments[len + 1];
+
+  fn = isFunction(fn) ? fn.bind.apply(fn, [null].concat(args)) : fn;
+  if (typeof Promise !== 'undefined') {
+    return resolved.then(fn);
+  }
+  var timerFunc = typeof requestAnimationFrame !== 'undefined' ? requestAnimationFrame : setTimeout;
+  timerFunc(fn);
 };
 
-// TODO: nexttick
-// TODO: options
-// TODO: PropTypes
-// TODO: getHooks ReactCurrentOwner.current
-// Current
-// Taro 2.x 会将函数式组件转换为类组件，所以要区分。
-// 这里要重写 Component 注入 Hooks？ withHooks？
-// module 形式
+// Current & getHooks
+Nerv.Current = Taro.Current;
+Nerv.getHooks = function getHooks(index) {
+  if (Taro.Current.current === null) {
+    throw new Error(`invalid hooks call: hooks can only be called in a stateless component.`);
+  }
+  const hooks = Taro.Current.current.hooks;
+  if (index >= hooks.length) {
+    hooks.push({});
+  }
+  return hooks[index];
+};
+
+// PropTypes
+const noop = () => {};
+var shim = noop;
+shim.isRequired = noop;
+function getShim() {
+  return shim;
+}
+var PropTypes = {
+  array: shim,
+  bool: shim,
+  func: shim,
+  number: shim,
+  object: shim,
+  string: shim,
+  any: shim,
+  arrayOf: getShim,
+  element: shim,
+  instanceOf: getShim,
+  node: shim,
+  objectOf: getShim,
+  oneOf: getShim,
+  oneOfType: getShim,
+  shape: getShim,
+  exact: getShim,
+  PropTypes: {},
+  checkPropTypes: noop,
+};
+PropTypes.PropTypes = PropTypes;
+Nerv.PropTypes = PropTypes;
 
 Nerv.default = Nerv;
 
